f76e7308c15aab15920aabd298307220c56d0f4f
Sumanth <sumanth@Sumanths-MacBook-Air.local>
2025-07-01 09:19:50 +0530

    Understand about FormData concept

---
+++ React.js/32-forms-in-react/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseTable from './components/ExpenseTable'
+import expenseData from '../expenseData'
+
+function App() {
+  const [expenses, setExpenses] = useState(expenseData)
+
+  return (
+      <main>
+      <h1>Track Your Expense</h1>
+      <div className="expense-tracker">
+        <ExpenseForm pullExpenses={setExpenses} />
+        <ExpenseTable expensesData={expenses} />
+        
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ React.js/32-forms-in-react/src/components/ExpenseForm.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function ExpenseForm({pullExpenses}) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const formData = new FormData(e.target);
+    // const data = {}
+
+    // for (const [key, value] of formData.entries()) {
+    //   data[key] = value
+  
+    // }
+    // console.log(data)
+    console.log(getFormData(e.target))
+    const setId = {...getFormData(e.target), id: crypto.randomUUID()}
+    pullExpenses((prevState) => [...prevState, setId])
+  };
+
+  const getFormData = (form) => {
+    console.log(form)
+       const formData = new FormData(form);
+    const data = {}
+
+    for (const [key, value] of formData.entries()) {
+      data[key] = value
+  
+    }
+    return data
+  }
+
+  return (
+    <form className="expense-form" onSubmit={handleSubmit}>
+      <div className="input-container">
+        <label htmlFor="title">Title</label>
+        <input id="title" name="title" />
+      </div>
+      <div className="input-container">
+        <label htmlFor="category">Category</label>
+        {/* <input id="category" /> */}
+        <select id="category" name="category">
+          <option value="" hidden>
+            Select Category
+          </option>
+          <option value="Grocery">Grocery</option>
+          <option value="Clothes">Clothes</option>
+          <option value="Bills">Bills</option>
+          <option value="Education">Education</option>
+          <option value="Medicine">Medicine</option>
+        </select>
+      </div>
+      <div className="input-container">
+        <label htmlFor="amount">Amount</label>
+        <input id="amount" name="amount"/>
+      </div>
+      <button className="add-btn">Add</button>
+    </form>
+  );
+}
